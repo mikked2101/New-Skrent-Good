21aa46f618cd194eea752d57893bfc357a069ddf
mikked2101 <78788768+mikked2101@users.noreply.github.com>
2023-02-05 18:47:37 +0100

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import './style.css';
 import Time from './Time';
-import Buses from './Buses';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <Time />
-      <Buses />
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
